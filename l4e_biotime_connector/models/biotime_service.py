5eb20b8fc5cc727d13fec51596cadbe63d090b56
Bharathikannan M <115794225+bharathi17656@users.noreply.github.com>
2026-02-06 20:04:34 +0530

    Update biotime_service.py

--- l4e_biotime_connector/models/biotime_service.py
+++ l4e_biotime_connector/models/biotime_service.py
@@ -384,7 +384,7 @@ class BiotimeService(models.Model):
     #             })
 
 
-    def _safe_paginated_get_line(self, start_url, username, password, max_pages=60):
+    def _safe_paginated_get_line(self, start_url, username, password, max_pages=250):
         url = start_url
         seen_urls = set()
         page = 0
@@ -551,6 +551,7 @@ class BiotimeService(models.Model):
                     'terminal_alias': tx.get("terminal_alias"),
                     'biotime_transaction_id': tx["id"],
                 })
+
 
 
 
