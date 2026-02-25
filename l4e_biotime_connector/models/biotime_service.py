569f5a49620518592354ca4573b7c4fe97ea9f24
Bharathikannan M <115794225+bharathi17656@users.noreply.github.com>
2026-02-25 21:27:44 +0530

    Update biotime_service.py

--- l4e_biotime_connector/models/biotime_service.py
+++ l4e_biotime_connector/models/biotime_service.py
@@ -212,7 +212,7 @@ class BiotimeService(models.Model):
     
     
 
-    def _safe_paginated_get_line_new(self,start_url,username,password,start_page=1,max_pages=50):
+    def _safe_paginated_get_line_new(self,start_url,username,password,start_page=1,max_pages=6):
         parsed = urlparse(start_url)
         query = parse_qs(parsed.query)
         query["page"] = [str(start_page)]
@@ -1073,6 +1073,7 @@ class BiotimeService(models.Model):
                 attendance.employee_id.id,
                 attendance.id,
             )
+
 
 
 
