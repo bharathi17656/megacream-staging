93532e41960b7598bfb5c72a5a3c465c06e4bb29
Bharathikannan M <115794225+bharathi17656@users.noreply.github.com>
2026-03-03 14:36:50 +0530

    Update hr_employee.py

--- adevx_hr_leave_rule/models/hr_employee.py
+++ adevx_hr_leave_rule/models/hr_employee.py
@@ -4,6 +4,6 @@ from odoo import models, fields
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-    esi_amount = fields.Float(string="ESI Amount")
-    bank_amount = fields.Float(string="Bank Salary Amount")
-    cash_amount = fields.Float(string="Cash Salary Amount")
+    esi_amount = fields.Monetary(string="ESI Amount")
+    bank_amount = fields.Monetary(string="Bank Salary Amount")
+    cash_amount = fields.Monetary(string="Cash Salary Amount")
