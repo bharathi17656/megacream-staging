c3ff03d87f1384d43f6b3d91f96a070e98410658
Bharathikannan M <115794225+bharathi17656@users.noreply.github.com>
2026-04-30 11:25:22 +0530

    Update __init__.py

--- adevx_hr_leave_rule/models/__init__.py
+++ adevx_hr_leave_rule/models/__init__.py
@@ -1,7 +1,7 @@
 
 from . import hr_leave_inherit
 from . import hr_leave_type
-from . import hr_working_schedule
+from . import hr_working_schedule 
 from . import hr_festival_holiday
 from . import hr_payslip 
 from . import hr_payslip_worked_days
