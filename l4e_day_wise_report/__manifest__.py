24a3609d493703b469c04d215933dccd006ef79f
suhail-L4E <suhail@links4engg.com>
2026-08-13 09:58:20 +0530

    day wise report update in purchase

--- l4e_day_wise_report/__manifest__.py
+++ l4e_day_wise_report/__manifest__.py
@@ -4,10 +4,11 @@
     'summary': 'Day-wise report menu for Sale Orders and Invoices',
     'category': 'Sales',
     'author': 'L4E',
-    'depends': ['sale', 'account'],
+    'depends': ['sale', 'account', 'purchase'],
     'data': [
         'views/sale_order_report_views.xml',
         'views/account_move_report_views.xml',
+        'views/purchase_bill_report_views.xml',
         'views/menu_views.xml',
     ],
     'installable': True,
