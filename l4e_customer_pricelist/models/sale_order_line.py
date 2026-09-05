549c5a9e57165fa88148302c168a81b4cbdb91e7
suhail-L4E <suhail@links4engg.com>
2026-09-05 11:43:14 +0530

    l4e_customer_pricelist update

--- l4e_customer_pricelist/models/sale_order_line.py
+++ l4e_customer_pricelist/models/sale_order_line.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models
+from odoo import api, models
 
 
 class SaleOrderLine(models.Model):
@@ -8,6 +8,9 @@ class SaleOrderLine(models.Model):
 
     def _compute_price_unit(self):
         super()._compute_price_unit()
+        self._apply_l4e_customer_price()
+
+    def _apply_l4e_customer_price(self):
         for line in self:
             partner = line.order_id.partner_id
             if not line.product_id or not partner:
@@ -21,3 +24,15 @@ class SaleOrderLine(models.Model):
             )
             if custom_price:
                 line.price_unit = custom_price.customer_price
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        lines = super().create(vals_list)
+        lines._apply_l4e_customer_price()
+        return lines
+
+    def write(self, vals):
+        res = super().write(vals)
+        if "product_id" in vals or "order_id" in vals:
+            self._apply_l4e_customer_price()
+        return res
